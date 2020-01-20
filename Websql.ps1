e6a480bad7bb8b4a8856f96cf6a590973d37e558
rupalimore1 <48875932+rupalimore1@users.noreply.github.com>
2020-01-20 11:50:19 +0530

    Update Websql.ps1

--- Websql.ps1
+++ Websql.ps1
@@ -1,6 +1,6 @@
 $rootFolder = $PSScriptRoot
 $templatePath = $rootFolder + '/websql.json'
-$templateParameterPath = $rootFolder + 'web.parameter.json'
+$templateParameterPath = $rootFolder + 'websql.parameter.json'
 
 
 New-AzResourceGroupDeployment -ResourceGroupName 'devopstraining1' -TemplateFile $templatePath -TemplateParameterFile $templateParameterPath 
