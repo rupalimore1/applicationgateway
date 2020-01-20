37577ba565cf669f154bd30b8102e8df35de3959
Rupali More <rupali.more@globant.com>
2020-01-20 11:25:33 +0530

    add

--- Websql.ps1
+++ Websql.ps1
@@ -1,3 +1,6 @@
-Connect-AzAccount
-Select-AzSubscription -SubscriptionId '202e1a05-a019-458e-9619-3d0325f48f8e'
-New-AzResourceGroupDeployment -ResourceGroupName 'devopstraining1' -TemplateFile .\websql.json -TemplateParameterFile .\web.parameter.json 
+$rootFolder = $PSScriptRoot
+$templatePath = $rootFolder + '/websql.json'
+$templateParameterPath = $rootFolder + 'web.parameter.json'
+
+
+New-AzResourceGroupDeployment -ResourceGroupName 'devopstraining1' -TemplateFile $templatePath -TemplateParameterFile $templateParameterPath 
